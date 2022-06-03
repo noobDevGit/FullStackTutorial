ccf0c0b12d2457ea94a916cda0d6de4c05a911e7
muhamad arifin ramadhan <muham@PLZNB7228300344.Supporting.corp.bankmandiri.co.id>
2022-06-03 17:38:50 +0700

    finished

--- server/index.js
+++ server/index.js
@@ -18,22 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",(req,res)=>{
-
-    const sqlInsert = "INSERT INTO contact (name, lastname, phone_num, reg_date) values ('Peter','Griffin','087662312',now())"
-    
-    db.query(sqlInsert,(error,result)=>{
-
-        console.log("error",error);
-        console.log('result',result);
-        res.send('success')
-
-    })
-
-})
 
 
-app.get('/customer',(req,res)=>{
+//get All Data
+app.get('/get/customer',(req,res)=>{
 
     const SqlgetContact = "select * from contact";
     
@@ -43,16 +31,26 @@ app.get('/customer',(req,res)=>{
 
 
     })
-
-
-
 })
 
-app.post('/post/customer',(req,res)=>{
+//get Data with specific id
+app.get('/get/customer/:id',(req,res)=>{
+    const{id} = req.params;
+    const SqlgetContact = "select * from contact where id = ?";
+    db.query(SqlgetContact, id, (error,result)=>{
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
+    })
+})
+
+//insert Data
+app.post('/api/post/customer',(req,res)=>{
 
     const {name, lastname, phone_num} = req.body
     const SqlCreateCustomer= "insert into contact (name, lastname, phone_num, reg_date) values (?, ?, ?, now())";
-    
+ 
     db.query(SqlCreateCustomer,[name, lastname, phone_num],(error,result)=>{
         if (error) {
 
@@ -62,6 +60,30 @@ app.post('/post/customer',(req,res)=>{
     })
 })
 
+
+//Update Data
+app.put('/put/customer/:id',(req,res)=>{
+    const{id} = req.params;
+    const {name, lastname, phone_num} = req.body
+    const SqlUpdate = "update contact set name = ?, lastname = ?, phone_num = ?  where id = ?";
+    db.query(SqlUpdate, [name, lastname, phone_num,id], (error,result)=>{
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
+    })
+})
+
+app.delete('/delete/customer/:id',(req,res)=>{
+    const{id} = req.params;
+    const SqlDeleteContact = "delete from contact where id = ?";
+    db.query(SqlDeleteContact, id, (error,result)=>{
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
+    })
+})
 
 app.listen(5000, ()=>{
 
